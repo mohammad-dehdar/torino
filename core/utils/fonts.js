6de231a53592bd7a59941dda9f52d32b3d15d1ab
mohammad dehdar <dehdardevelop@gmail.com>
2024-12-23 10:26:21 +0330

    config api

---
+++ core/utils/fonts.js
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+
+export const yekan = localFont({
+  src: [
+    {
+      path: "../../app/fonts/YekanBakh-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/YekanBakh-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/YekanBakh-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/YekanBakh-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/YekanBakh-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../app/fonts/YekanBakh-Heavy.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
